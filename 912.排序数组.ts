78b463a840dfc3953df16fc9419905db22307170
zhouchang <xiaochangvv@gmail.com>
2023-06-28 15:19:01 +0800

    feat: 排序数组代码

---
+++ 912.排序数组.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=912 lang=typescript
+ *
+ * [912] 排序数组
+ */
+
+// @lc code=start
+function sortArray(nums: number[]): number[] {
+  return nums.sort((a, b) => a - b);
+}
+
+// @lc code=end
